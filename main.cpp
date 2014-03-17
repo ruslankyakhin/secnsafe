a39f2e5d3a723cdefc74ffd77f6f69124f1d35ec
ruslankyakhin <ruslan.k.yakhin@gmail.com>
2014-03-17 23:12:48 +0400

    test

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
     CFilesAnalize *analizator = new CFilesAnalize();
     CReport *report           = new CReport();
 
-
+    //жопа
     //CSettings::instance().loadSettings("/etc/secnsafe/secnsafe.conf");
     //CSettings::instance().setValue("dbfolder","/etc/secnsafe"
 
